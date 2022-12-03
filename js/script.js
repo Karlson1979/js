07c03e449633eef5e403fe12349da337ba144517
Karlson1979 <113626246+Karlson1979@users.noreply.github.com>
2022-12-03 12:32:35 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//1
+var x=confirm('Ви повнолітня особа?');
+  
+if(x)alert('Доступ до сайту відкритий');
+else alert('Доступ до сайту закритий');
+
+//2
+var x=prompt('Ведить Ваш пароль');
+var t=Step
+var y=Web;
+var z=JavaScript
+if(t,y,z)alert('Підтверджено');
+else alert('Скасовано');
+
+
+
+
