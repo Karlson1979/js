cce44fe2f9f76a94791f1a1b7cb9449d19ab6837
Karlson1979 <113626246+Karlson1979@users.noreply.github.com>
2022-12-12 22:09:34 +0200

    1-2

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// //1
+// x=+prompt('Вкажить свій вік:');
+// if(x>=0&&x<=2) alert('Ви дитина');
+// if(x>=12&&x<18) alert('Ви підліток');
+// if(x>=18&&x<=59) alert('Ви дорослий');
+// if(x>=60) alert('Ви пенсіонер');
+
+//2
+// x=+prompt('Вкажить число від 0 до 9:');
+// switch(x){
+//     case 0: alert(')');
+//     break;
+//     case 1: alert('!');
+//     break;
+//     case 2: alert('@');
+//     break;
+//     case 3: alert('#');
+//     break;
+//     case 4: alert('$');
+//     break; 
+//     case 5: alert('%');
+//     break;
+//     case 6: alert('^');
+//     break;
+//     case 7: alert('&');
+//     break;
+//     case 8: alert('*');
+//     break;
+//     case 9: alert('(');
+//     break; 
+
+//     default: alert('не вірно вказано число');
+// }
+
+//3
+x=+prompt('введить трьохзначне число:');
+y=+prompt('введить число:');
