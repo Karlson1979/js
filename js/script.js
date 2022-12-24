8120060a2f571d1c5a9b02117b5ccef6dca7f8b0
Karlson1979 <113626246+Karlson1979@users.noreply.github.com>
2022-12-24 12:03:38 +0200

    1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//1
+var a = +prompt("введіть початок діапазону");
+var n = +prompt("введіть кінець діапазону");
+var sum = 0;
+ 
+while(a <= n){       
+       sum+=a;  
+       a++;
+    }
+ 
+alert(sum);
