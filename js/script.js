df549d4f7618dd942695adcd1c1f8fcbde878e7f
Karlson1979 <113626246+Karlson1979@users.noreply.github.com>
2022-12-30 23:21:02 +0200

    2

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ while(a <= n){
     }
  
 alert(sum);
+
+//2
+var a = +prompt('ввведить перше число')
+var b = +prompt('ввведить друге число')
+var gcd;
+while (a!=b) {
+  if (a>b) {
+    a = a -b;
+  }
+  else {
+    b = b - a;
+  }
+}
+gcd = a;
+alert(gcd);
+
+//3
