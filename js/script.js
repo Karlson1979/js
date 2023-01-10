6da305df176729ee56d37bb961a91617b15aa112
Karlson1979 <113626246+Karlson1979@users.noreply.github.com>
2023-01-10 18:18:48 +0200

    dz7-4

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//1
+// var x=+prompt('введить число');
+// var y=+prompt('введить число');
+// function fun(x,y){
+//     if(x<y)
+//     return -1;
+//     else if(x>y)
+//     return 1;
+//     else return 0;
+// }
+
+// alert (fun(x,y));
+
+//2
+// var x=+prompt('введить число');
+// function fun(x){
+//     var i=1;
+//     var res=1
+// while (i<=x) {
+// res*=i;
+// ++i;
+
+// }
+
+// alert(res)};
+// fun(x);
+
+//3
+// var a= prompt('введить цифру');
+// var b= prompt('введить цифру');
+// var c= prompt('введить цифру');
+// function fun (a,b,c){
+//     var res= a+b+c;
+// var res= parseInt(res);
+// alert(res);
+// }
+
+
+// fun(a,b,c);
