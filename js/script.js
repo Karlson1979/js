720813ec3127dd197d09a0ec7fef83abf1e64bef
Karlson1979 <113626246+Karlson1979@users.noreply.github.com>
2023-01-11 13:06:26 +0200

    1-3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-const a=100000;
-const b=100000000;
-const c=`${a}`+`${b}`;
-// console.log(`${a}`+`${b}`);
-console.log(c.length);
+//1
+    // const a = 100000;
+    // const b = 100000000;
+    // const c = `${a}` + `${b}`;
+    // console.log(c.length + ' - довжина кода');
+
+//2
+// let a = 'Hello World';
+// console.log(a.replaceAll ('Hello World','World Hello'));
+
+//3
+// let a="Test string, .., -- string test";
+// console.log(a.slice(0,10)+ a.slice(19,a.length));
+
