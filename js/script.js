18049bf373d3670adca3881fbe13810ae81ecef2
Karlson1979 <113626246+Karlson1979@users.noreply.github.com>
2023-01-11 09:17:36 +0200

    lesson-1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// let a=10;
+// let b=30;
+// let c=20;
+
+// console.log (a+b+c);
+// console.log (a*b*c);
+// console.log (a/b/c);
+// console.log (a-b+c);
+
+
+
---
+++ js/string.js
@@ -0,0 +1,16 @@
+// const string='';
+// console.log(string.length);
+// let sentence = 'JavaScript, TypeScript, CoffeScript'
+// console.log(sentence.replaceAll(',', '...'));
+// let a='javaScript';
+// console.log(a.replace(slice(2,1),'J'));
+
+const a = "javaScript";
+
+const b = "typeScript";
+
+
+
+console.log(b[0].slice() + a.slice(1, a.length))
+
+console.log(a[0].slice() + b.slice(1, b.length))
